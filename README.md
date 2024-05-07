2aed7082454f642b4721351233f9821c6695cfb7
SB1019 <sasrinath@ucsd.edu>
2024-05-06 23:13:09 -0700

    Finished Unit Tests and Readme regarding Unit Tests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 5 - Starter
+
+Contributers: Andrew Yang and Samvrit Srinath
+
+## Check your Understanding - Unit Tests
+
+1. We would not use a Unit Test to test the `message` feature of our messaging application. To boil it down, the feature is simply just far too large to test using Unit Tests. There are many moving parts to the message user which include: parsing the input, queuing the message, sending the message, receiving the message, displaying the message to the recipent. This is a lot of functionality to test and it would be very difficult to test all of these features using Unit Tests. Also, this task would not execute quickly, but rather on orders of magnitude slower than a typical unit test(waiting for network and recipient to acknowledge the message). Also, the message feature is not able to be isolated, many different parts of the program can impact the behavior of the message feature. Thus, since the message feature is neither small, nor isolated, nor fast, it doesn't make sense to test it using Unit Tests.
+
+2. We would use unit tests to test the `max_message_length` feature of our messaging application. This feature is small and isolated, and can be tested quickly. The `max_message_length` feature is a simple boolean check that can be tested by passing in a message that is too long and a message that is within the length limit(80 Characters). This feature is also isolated, as it is modular with respect to the rest of the program. Due to these reasons, this feature being small, fast to test, and isolated from the rest of the program(as this function is slot in to impact the `message` behavior and has no external dependencies), it makes sense to test this feature using Unit Tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,11 +1,124 @@
 // unit.test.js
 
 import {
-  isPhoneNumber,
-  isEmail,
-  isStrongPassword,
-  isDate,
-  isHexColor,
-} from '../code-to-unit-test/unit-test-me';
+	isPhoneNumber,
+	isEmail,
+	isStrongPassword,
+	isDate,
+	isHexColor,
+} from "../code-to-unit-test/unit-test-me";
 
-// TODO - Part 2
+//tests for isPhone Number
+//true cases
+
+test("isPhoneNumber: 555-555-5555 should return true", () => {
+	expect(isPhoneNumber("555-555-5555")).toBe(true);
+});
+
+test("isPhoneNumber: (921) 565-3423 should return true", () => {
+	expect(isPhoneNumber("(921) 565-3423")).toBe(true);
+});
+
+//false cases
+
+test("isPhoneNumber: alphanumeric characters and incorrect length should return false", () => {
+	expect(isPhoneNumber("abc123")).toBe(false);
+});
+
+test("isPhoneNumber: 123-456-789, incorrect length:  should return false", () => {
+	expect(isPhoneNumber("123-456-789")).toBe(false);
+});
+
+//test isEmail
+//true cases
+//ucsd email
+test("isEmail: sasrinath@ucsd.edu should return true", () => {
+	expect(isEmail("sasrinath@ucsd.edu")).toBe(true);
+});
+
+test("isEmail: generic .com extension should return true", () => {
+	expect(isEmail("iLovemyCountry@email.com")).toBe(true);
+});
+
+//false cases
+//custom country extension doesn't match regex
+test("isEmail: custom country extension should return false", () => {
+	expect(isEmail("tw@icplsw.co.tw")).toBe(false);
+});
+
+//missing @ symbol
+test("isEmail: missing @ symbol should return false", () => {
+	expect(isEmail("twicplsw.co.tw")).toBe(false);
+});
+
+//test isStrongPassword
+//true cases
+
+//lowercase start, 3 letters, 1 number
+test("isStrongPassword: abc1 should return true", () => {
+	expect(isStrongPassword("abc1")).toBe(true);
+});
+
+//uppercase start, 14 letters
+test("isStrongPassword: Abcdefghijklmnop should return true", () => {
+	expect(isStrongPassword("Abcdefghijklmno")).toBe(true);
+});
+
+//false cases
+//incorrect length(2 characters and starts with a number)
+test("isStrongPassword: 1a should return false", () => {
+	expect(isStrongPassword("1a")).toBe(false);
+});
+
+//incorrect length(16 characters)
+test("isStrongPassword: 1234567890123456 should return false", () => {
+	expect(isStrongPassword("1234567890123456")).toBe(false);
+});
+
+//test isDate
+
+//true cases
+//current date
+test("isDate: 05/06/2024 should return true", () => {
+	expect(isDate("05/06/2024")).toBe(true);
+});
+
+//test old birthday
+test("isDate: 6/8/1990 should return true", () => {
+	expect(isDate("6/8/1990")).toBe(true);
+});
+
+//false cases
+//incorrect year
+test("isDate: 05/06/24 should return false", () => {
+	expect(isDate("05/06/24")).toBe(false);
+});
+
+//incorrect seperators
+test("isDate: 05-06-2024 should return false", () => {
+	expect(isDate("05-06-2024")).toBe(false);
+});
+
+//test isHexColor
+
+//true cases
+//3 color hex with letters
+test("isHexColor: #fff should return true", () => {
+	expect(isHexColor("#fff")).toBe(true);
+});
+
+//6 color hex with numbers
+test("isHexColor: 000000 should return true", () => {
+	expect(isHexColor("000000")).toBe(true);
+});
+
+//false cases
+//incorrect length
+test("isHexColor: fff1236 should return false", () => {
+	expect(isHexColor("fff1236")).toBe(false);
+});
+
+//uses invalid letters
+test("isHexColor: #gggggg should return false", () => {
+	expect(isHexColor("#gggggg")).toBe(false);
+});
